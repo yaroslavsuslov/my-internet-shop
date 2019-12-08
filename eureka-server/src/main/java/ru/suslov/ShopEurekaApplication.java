606b591b196b1ad3cb0423794b2f1d3608da34b0
yaroslavsuslov <yaroslavsuslov@gmail.com>
2019-12-09 00:12:16 +0300

    zuul and Eureka

---
+++ eureka-server/src/main/java/ru/suslov/ShopEurekaApplication.java
@@ -0,0 +1,15 @@
+package ru.suslov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ShopEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopEurekaApplication.class, args);
+    }
+
+}
