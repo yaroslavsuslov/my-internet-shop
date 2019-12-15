5ee7fe1a9f5ec93b0c943b27be1001e574c13a24
yaroslavsuslov <yaroslavsuslov@gmail.com>
2019-12-15 21:06:50 +0300

    docker

--- shop-ui/src/main/java/com/suslov/SessionConfig.java
+++ shop-ui/src/main/java/com/suslov/SessionConfig.java
@@ -1,19 +1,19 @@
-package com.suslov;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-
-@Configuration
-@EnableRedisHttpSession
-public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
-
-    @Bean
-    RedisSerializer<Object> springSessionDefaultRedisSerializer() {
-        return new GenericJackson2JsonRedisSerializer();
-    }
-}
+//package com.suslov;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.RedisSerializer;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+//
+//
+//@Configuration
+//@EnableRedisHttpSession
+//public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
+//
+//    @Bean
+//    RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+//        return new GenericJackson2JsonRedisSerializer();
+//    }
+//}
